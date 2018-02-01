eb4340f9321def8c6eeed133c37eb35efbebd622
NIC-SURESH <NIC-SURESH@ShrutiNIC>
2018-02-01 12:57:36 +0530

    sub class is added

---
+++ src/com/sample/Sub.java
@@ -0,0 +1,18 @@
+package com.sample;
+
+public class Sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a=10000;
+		for(int i=1;i<=a;i++)
+		{
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
