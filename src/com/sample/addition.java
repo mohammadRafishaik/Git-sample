14194d97f4b5d21da3bcb2d906d2d057afb36181
mmarjun040 <mmarjun040@NIC>
2018-02-01 12:01:56 +0530

    sample project for git test

---
+++ src/com/sample/addition.java
@@ -0,0 +1,19 @@
+package com.sample;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a=100;
+		for(int i=1;i<=a;i++)
+		{
+			if(i%2==0) {
+				System.out.println(" value of even "+i);
+			}
+		}
+
+	}
+
+}
