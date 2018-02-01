5f0b906e5a8f608d493087e96333279dd13508b4
mmarjun040 <mmarjun040@NIC>
2018-02-01 12:12:51 +0530

    even/add/1000

---
+++ src/com/sample/even_add.java
@@ -0,0 +1,24 @@
+package com.sample;
+
+public class even_add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1000;
+		for(int i=1;i<=a;i++) {
+			
+		//	System.out.println(i);
+			if(i%2==0) {
+				System.out.println(" values "+i);
+			}
+			else
+			{
+				if(i%2!=0) {
+					System.out.println(i);
+				}
+			}
+		}
+
+	}
+
+}
